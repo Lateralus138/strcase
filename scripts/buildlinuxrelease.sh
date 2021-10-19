a7d3a4b4e457f9f6f35d8b2d52cbec47bdf3fd27
Lateralus138 <faithnomoread@yahoo.com>
2021-10-18 23:18:54 -0500

    Uploading initial project files, first tests - Mon 18 Oct 2021 11:18:54 PM CDT

--- scripts/buildlinuxrelease.sh
+++ scripts/buildlinuxrelease.sh
@@ -2,8 +2,8 @@
 cd "$(dirname $0)"
 error=1
 # error=2
-root='..'
-source="${root}/linux/src"
+root='../linux'
+source="${root}/src"
 lib="${source}/lib/linux"
 include="${source}/include"
 # libs=( functions ProgramErrors pe )
@@ -11,11 +11,12 @@ error=$((error + 1))
 [[ ! -d "$root" ]] && exit $error
 error=$((error + 1))
 [[ ! -d "$source" ]] && exit $error
+error=$((error + 1))
+[[ ! -x "${source}/main.cpp" ]] && exit $error
 # error=$((error + 1))
 # [[ ! -d "$lib" ]] && exit $error
 # error=$((error + 1))
 # [[ ! -d "$include" ]] && exit $error
-tree -C
 # for libv in "${libs[@]}"; do
 #   error=$((error + 1))
 #   if [[ ! -f "${lib}/${libv}.o" ]]; then
@@ -27,17 +28,16 @@ tree -C
 #   fi
 # done
 
-# cd "${root}"
-# error=$((error + 1))
-# if make cleantest test; then
-#   make cleantest
-#   error=$((error + 1))
-#   if make cleandebug debug; then
-#     make cleandebug
-#     error=$((error + 1))
-#     if make; then
-#       printf '%s\n' "All MAKE tasks completed successfully."
-#       cd "${root}"
-#     else exit $error; fi
-#   else exit $error; fi
-# else exit $error; fi
+cd "${root}"
+error=$((error + 1))
+make buildlinuxobjects && make buildlinuxarchive && make releasedynamiclinux
+if make buildlinuxobjects; then
+  error=$((error + 1))
+  if make buildlinuxarchive; then
+    error=$((error + 1))
+    if make releasedynamiclinux; then
+      printf '%s\n' "All MAKE tasks completed successfully."
+      cd "${root}"
+    else exit $error; fi
+  else exit $error; fi
+else exit $error; fi
