9a2a07ce4e872abe0a3d90d3595c699e126f9179
Lateralus138 <faithnomoread@yahoo.com>
2021-10-19 20:51:03 -0500

    Uploading initial project files, first tests - Tue 19 Oct 2021 08:51:03 PM CDT

--- scripts/buildwindowsrelease.ps1
+++ scripts/buildwindowsrelease.ps1
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 $error_ = 38
 $this_path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$released = "$base\release\windows"
 cd "$this_path"
 $base = ".."
-$root = "$base/windows"
+$root = "$base\windows"
 $error_++
 Test-Path "$root" -PathType Container || Exit $error_
+if (-not (Test-Path "$released" -PathType Container))
+{
+  $error_++
+  New-Item -Type Directory -Name "$released"  || Exit $error_
+}
 cd "${root}"
 $error_++
 MSBuild /property:Configuration=Release /property:Platform=x86 || Exit $error_
 $error_++
+Move-Item -Force -Confirm -Path "Release\strcase.exe" "$released/strcase-x86.exe" || Exit $error_
+$error_++
 MSBuild /property:Configuration=Release /property:Platform=x64 || Exit $error_
+$error_++
+Move-Item -Force -Confirm -Path "x64\Release\strcase.exe" "$released/strcase-x64.exe" || Exit $error_
 Tree /F "$root"
+Tree /F "$base/release/windows"
