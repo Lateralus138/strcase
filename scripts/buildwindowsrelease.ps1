1da664097d63298ef4e6edfc173637c849370ef6
Lateralus138 <faithnomoread@yahoo.com>
2021-10-19 21:23:44 -0500

    Uploading initial project files, first tests - Tue 19 Oct 2021 09:23:43 PM CDT

--- scripts/buildwindowsrelease.ps1
+++ scripts/buildwindowsrelease.ps1
@@ -21,10 +21,10 @@ $error_++
 Test-Path ".\Release\strcase.exe" -PathType Container || Exit $error_
 Write-Host "<- $MyInvocation.ScriptLineNumber"
 $error_++
-Move-Item -Path ".\Release\strcase.exe" "$released\strcase-x86.exe" -Force || Exit $error_
+Move-Item -Path ".\Release\strcase.exe" -Destination "$released\strcase-x86.exe" -Force || Exit $error_
 $error_++
 MSBuild /property:Configuration=Release /property:Platform=x64 || Exit $error_
 $error_++
-Move-Item -Path ".\x64\Release\strcase.exe" "$released\strcase-x64.exe" -Force || Exit $error_
+Move-Item -Path ".\x64\Release\strcase.exe" -Destination "$released\strcase-x64.exe" -Force || Exit $error_
 Tree /F "$root"
 Tree /F "$base/release/windows"
