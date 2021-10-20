3c984216fd06f418c657a08435bded0401640afe
Lateralus138 <faithnomoread@yahoo.com>
2021-10-19 21:12:44 -0500

    Uploading initial project files, first tests - Tue 19 Oct 2021 09:12:44 PM CDT

--- scripts/buildwindowsrelease.ps1
+++ scripts/buildwindowsrelease.ps1
@@ -7,20 +7,23 @@ $base = ".."
 $root = "$base\windows"
 $error_++
 Test-Path "$root" -PathType Container || Exit $error_
-if (-not (Test-Path "$($released)" -PathType Container))
+if (-not (Test-Path "$released" -PathType Container))
 {
   $error_++
-  New-Item -Type Directory -Name "$($released)"  || Exit $error_
+  New-Item -Type Directory -Name "$released"  || Exit $error_
 }
+Write-Host $MyInvocation.ScriptLineNumber
 cd "${root}"
 $error_++
 MSBuild /property:Configuration=Release /property:Platform=x86 || Exit $error_
 Tree /F "$root"
 $error_++
-Move-Item -Path ".\Release\strcase.exe" "$($released)\strcase-x86.exe" -Force || Exit $error_
+Test-Path ".\Release\strcase.exe" -PathType Container || Exit $error_
+$error_++
+Move-Item -Path ".\Release\strcase.exe" "$released\strcase-x86.exe" -Force || Exit $error_
 $error_++
 MSBuild /property:Configuration=Release /property:Platform=x64 || Exit $error_
 $error_++
-Move-Item -Path ".\x64\Release\strcase.exe" "$($released)\strcase-x64.exe" -Force || Exit $error_
+Move-Item -Path ".\x64\Release\strcase.exe" "$released\strcase-x64.exe" -Force || Exit $error_
 Tree /F "$root"
 Tree /F "$base/release/windows"
